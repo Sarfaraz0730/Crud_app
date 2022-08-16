d69df5c5125a5e2647c576e20594aeb8b284ff7d
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-08-16 20:01:53 +0530

    setting up ejs , successfully loaded on web

--- Crud_Table/server.js
+++ Crud_Table/server.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const path = require("path");
+
+
 const app = express();
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 
+//log request
+app.use(morgan('tiny'))
+//parse request to body parser
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname,"views/ejs"))
+
+
+// load assets using  middleware method
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/css", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/css", express.static(path.resolve(__dirname, "assets/js")));
+
+
 app.get("/", (req, res) => {
   console.log("get request");
-  res.send("Crud App");
+res.render("index.ejs")
 });
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
