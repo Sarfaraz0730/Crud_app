ba85520514504f5b024308408403546477fa51c5
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-08-17 23:55:50 +0530

    update and new user page created Working on it's functionality

--- Crud_Table/server.js
+++ Crud_Table/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
-const bodyparser = require('body-parser')
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
 const path = require("path");
-
 
 const app = express();
 
@@ -11,7 +10,7 @@ dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 
 //log request
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 //parse request to body parser
 
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -21,17 +20,25 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 // app.set("views", path.resolve(__dirname,"views/ejs"))
 
-
 // load assets using  middleware method
 
 app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/css", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/css", express.static(path.resolve(__dirname, "assets/js")));
 
-
 app.get("/", (req, res) => {
-  console.log("get request");
-res.render("index.ejs")
+  console.log(" home page  request");
+  res.render("index.ejs");
+});
+
+app.get("/add-user", (req, res) => {
+  console.log("Got request of add-user");
+  res.render("add_user");
+});
+
+app.get("/update-user", (req, res) => {
+  console.log("Got request of update-user");
+  res.render("update_user");
 });
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
